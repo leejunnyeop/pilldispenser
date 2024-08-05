f5f6dcb809a271910b6b2bea0456244ad5c9d05c
leejunnyeop <daffodil8620@naver.com>
2024-08-05 18:34:50 +0900

    first commit

---
+++ src/main/java/gist/pilldispenser/PilldispenserApplication.java
@@ -0,0 +1,13 @@
+package gist.pilldispenser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PilldispenserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PilldispenserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gist/pilldispenser/PilldispenserApplicationTests.java
@@ -0,0 +1,13 @@
+package gist.pilldispenser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PilldispenserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
