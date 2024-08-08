abec21873e69fcfe94a9169e9b4a14204f541e53
leejunnyeop <daffodil8620@naver.com>
2024-08-08 13:10:08 +0900

    [refactor] 글로벌 에러

---
+++ src/main/java/gist/pilldispenser/global/ex/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package gist.pilldispenser.global.ex;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DrugInfoNotFoundException.class)
+    public ResponseEntity<String> handleDrugInfoNotFoundException(DrugInfoNotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("서버 오류가 발생했습니다. 잠시 후 다시 시도해 주세요.");
+    }
+}
