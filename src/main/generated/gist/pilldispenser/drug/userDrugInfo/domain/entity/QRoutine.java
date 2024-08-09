44f433747f196d1b3bc87e6f3ed9a4e55ed4043c
leejunnyeop <daffodil8620@naver.com>
2024-08-09 15:48:26 +0900

    [fix] Q class

---
+++ src/main/generated/gist/pilldispenser/drug/userDrugInfo/domain/entity/QRoutine.java
@@ -0,0 +1,59 @@
+package gist.pilldispenser.drug.userDrugInfo.domain.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+import com.querydsl.core.types.dsl.PathInits;
+
+
+/**
+ * QRoutine is a Querydsl query type for Routine
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QRoutine extends EntityPathBase<Routine> {
+
+    private static final long serialVersionUID = -1159542017L;
+
+    private static final PathInits INITS = PathInits.DIRECT2;
+
+    public static final QRoutine routine = new QRoutine("routine");
+
+    public final NumberPath<Integer> dailyDosage = createNumber("dailyDosage", Integer.class);
+
+    public final NumberPath<Integer> dosagePerTake = createNumber("dosagePerTake", Integer.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final BooleanPath isActive = createBoolean("isActive");
+
+    public final TimePath<java.time.LocalTime> time = createTime("time", java.time.LocalTime.class);
+
+    public final QUserDrugInfo userDrugInfo;
+
+    public QRoutine(String variable) {
+        this(Routine.class, forVariable(variable), INITS);
+    }
+
+    public QRoutine(Path<? extends Routine> path) {
+        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+    }
+
+    public QRoutine(PathMetadata metadata) {
+        this(metadata, PathInits.getFor(metadata, INITS));
+    }
+
+    public QRoutine(PathMetadata metadata, PathInits inits) {
+        this(Routine.class, metadata, inits);
+    }
+
+    public QRoutine(Class<? extends Routine> type, PathMetadata metadata, PathInits inits) {
+        super(type, metadata, inits);
+        this.userDrugInfo = inits.isInitialized("userDrugInfo") ? new QUserDrugInfo(forProperty("userDrugInfo"), inits.get("userDrugInfo")) : null;
+    }
+
+}
+
---
+++ src/main/generated/gist/pilldispenser/users/domain/entity/QUsers.java
@@ -0,0 +1,58 @@
+package gist.pilldispenser.users.domain.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QUsers is a Querydsl query type for Users
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QUsers extends EntityPathBase<Users> {
+
+    private static final long serialVersionUID = -817299932L;
+
+    public static final QUsers users = new QUsers("users");
+
+    public final gist.pilldispenser.common.entity.QBaseEntity _super = new gist.pilldispenser.common.entity.QBaseEntity(this);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createdAt = _super.createdAt;
+
+    public final StringPath email = createString("email");
+
+    public final StringPath hardwareNo = createString("hardwareNo");
+
+    //inherited
+    public final NumberPath<Long> id = _super.id;
+
+    public final StringPath nickname = createString("nickname");
+
+    public final StringPath password = createString("password");
+
+    public final StringPath profileImage = createString("profileImage");
+
+    public final EnumPath<gist.pilldispenser.common.entity.enums.RoleType> roleType = createEnum("roleType", gist.pilldispenser.common.entity.enums.RoleType.class);
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = _super.updatedAt;
+
+    public QUsers(String variable) {
+        super(Users.class, forVariable(variable));
+    }
+
+    public QUsers(Path<? extends Users> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QUsers(PathMetadata metadata) {
+        super(Users.class, metadata);
+    }
+
+}
+
