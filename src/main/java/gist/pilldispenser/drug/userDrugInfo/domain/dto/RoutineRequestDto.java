ff2c099d6efb6b17907aab7cf8010b653defba68
c0smosaur <sjkang539@gmail.com>
2024-08-11 00:08:56 +0900

    [fix] dto 생성자 추가
    - RoutineRequestDto: 생성자 추가하여 JSON parsing error 수정

--- src/main/java/gist/pilldispenser/drug/userDrugInfo/domain/dto/RoutineRequestDto.java
+++ src/main/java/gist/pilldispenser/drug/userDrugInfo/domain/dto/RoutineRequestDto.java
@@ -1,26 +1,31 @@
 package gist.pilldispenser.drug.userDrugInfo.domain.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalTime;
 
 @Getter
-@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class RoutineRequestDto {
 
     @Schema(description = "복용 시간", example = "08:00")
-    private LocalTime time;  // 복용 시간
+    @JsonFormat(pattern = "HH:mm")
+    private String time;  // 복용 시간
 
     @Schema(description = "복용 시 1회 복용량", example = "2")
-    private int dosagePerTake;  // 복용 시 1회 복용량
+    private Integer dosagePerTake;  // 복용 시 1회 복용량
 
     @Schema(description = "하루 복용 횟수", example = "3")
-    private int dailyDosage;  // 하루 복용 횟수
+    private Integer dailyDosage;  // 하루 복용 횟수
 
     @Schema(description = "루틴 활성화 여부", example = "true")
-    private boolean isActive;  // 루틴 활성화 여부
+    private Boolean isActive;  // 루틴 활성화 여부
 
     @Schema(description = "약물 고유 번호 (itemSeq)", example = "123456789")
     private String itemSeq;  // 약물 고유 번호
