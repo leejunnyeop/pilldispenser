0b84508f0d53262cfd6c7ab9d3ca2a507b22a14c
ndy <ratcomp9992@gmail.com>
2024-08-13 01:50:58 +0900

    [feat] 디자인 변경에 따른 Response 수정

--- src/main/java/gist/pilldispenser/drug/userDrugInfo/domain/dto/UserDrugRoutineResponse.java
+++ src/main/java/gist/pilldispenser/drug/userDrugInfo/domain/dto/UserDrugRoutineResponse.java
@@ -1,22 +1,14 @@
 package gist.pilldispenser.drug.userDrugInfo.domain.dto;
 
-import gist.pilldispenser.drug.userDrugInfo.domain.enums.DayType;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import lombok.Builder;
 import lombok.Getter;
-
-import java.time.DayOfWeek;
-import java.time.LocalTime;
 
 @Getter
 @Builder
 public class UserDrugRoutineResponse {
 
     private String drugName;       // 약물 이름
-    private int dosagePerTake;     // 복용 시 1회 복용량
-
-    private DayType days;
+    private String entrps;          // 업체명
 
 
 }
